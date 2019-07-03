51acaf9f810d15a83b99fbfb690adc772f6bc854
fangfujing@aliyun.com <fangfujing@alilyun.com>
2019-07-03 16:51:17 +0800

    444

--- src/main/java/mybaits/jdbc/JDBCTest.java
+++ src/main/java/mybaits/jdbc/JDBCTest.java
@@ -11,14 +11,29 @@ public class JDBCTest {
             Class.forName("oracle.jdbc.OracleDriver");
             connection = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:XE",
                     "weblogic", "weblogic1");
-            ps = connection.prepareStatement("select * from menu where id = ?");
+            //query
+//            ps = connection.prepareStatement("select * from menu where id = ?");
+//            ps.setString(1, "1234");
+//            ResultSet rs = ps.executeQuery();
+//            while (rs.next()){
+//                System.out.println(rs.getString(1)+rs.getString(2)+rs.getString(3)+rs.getInt(4));
+//            }
 
-            ps.setString(1, "1234");
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()){
-                System.out.println(rs.getString(1)+rs.getString(2)+rs.getString(3)+rs.getInt(4));
-            }
 
+            //insert
+//            connection.setAutoCommit(false);
+//            String sql = "insert into t_stu values (?)";
+//            ps = connection.prepareStatement(sql);
+//            ps.setString(1, null);
+//            System.out.println(ps.executeUpdate());
+
+            String sql = "insert into emp (emp_no, create_time) values (?,?)";
+            ps = connection.prepareStatement(sql);
+            ps.setInt(1, 4);
+            Date date = new Date(System.currentTimeMillis());
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            ps.setTimestamp(2, timestamp);
+            System.out.println(ps.executeUpdate());
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
@@ -39,7 +54,6 @@ public class JDBCTest {
                 }
             }
         }
-
 
     }
 }
