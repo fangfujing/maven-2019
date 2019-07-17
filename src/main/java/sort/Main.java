de26390c27af08b0d0f088c56fbbcf0b88139fdf
fangfujing@aliyun.com <fangfujing@alilyun.com>
2019-07-17 14:31:29 +0800

    444

---
+++ src/main/java/sort/Main.java
@@ -0,0 +1,17 @@
+package sort;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        HashMap<String, String > hashMap = new HashMap<>(100);
+        hashMap.put("aa", "bb");
+
+        hashMap.get("aa");
+
+
+    }
+}
