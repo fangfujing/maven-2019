939de8a5e874874ebb2cac4dc7daf52c4a6ac91c
fangfujing@aliyun.com <fangfujing@alilyun.com>
2019-08-01 16:08:20 +0800

    444

--- src/main/java/hashmap/Main.java
+++ src/main/java/hashmap/Main.java
@@ -5,19 +5,32 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
-        HashMap<Stu, String> map = new HashMap<>();
+    HashMap<Integer, Integer> map = new HashMap<>();
 
-        Stu s = new Stu();
-        s.setAge(1);
-        s.setName("bb");
-        Stu s2 = new Stu();
-        s2.setAge(12);
-        s2.setName("bb");
-
-        System.out.println(s.hashCode());
-        System.out.println(s2.hashCode());
+//        Stu s = new Stu();
+//        s.setAge(1);
+//        s.setName("bb");
+//        Stu s2 = new Stu();
+//        s2.setAge(12);
+//        s2.setName("bb");
+//
+//        System.out.println(s.hashCode());
+//        System.out.println(s2.hashCode());
 //        map.put(s, "b");
-
 //        System.out.println(map.get(s));
+
+        for (int i = 1; i <= 16; i++) {
+            map.put(i, i);
+        }
+
+
+//        System.out.println(map);
+//        System.out.println(map2);
+
+//        test();
+
+    }
+
+    public static void test(){
     }
 }
