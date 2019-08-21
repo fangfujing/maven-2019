51d4e570b547da127d434bcadc9ef67b720fac35
fangfujing@aliyun.com <fangfujing@aliyun.com>
2019-08-21 09:37:52 +0800

    444

---
+++ src/main/java/thread/Counter.java
@@ -0,0 +1,19 @@
+package thread;
+
+public class Counter {
+
+    public volatile static int count = 0;
+
+    public static void inc() {
+
+        //这里延迟1毫秒，使得结果明显
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+        }
+
+        count++;
+    }
+
+
+}
