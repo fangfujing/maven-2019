d5a0959acda32b792f82c106e742ca1b48f6e1be
fangfujing@aliyun.com <fangfujing@aliyun.com>
2019-08-21 09:33:06 +0800

    444

--- src/main/java/hashmap/Main.java
+++ src/main/java/hashmap/Main.java
@@ -1,11 +1,36 @@
 package hashmap;
 
-import java.util.HashMap;
+import com.alibaba.fastjson.JSON;
+
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-    HashMap<Integer, Integer> map = new HashMap<>();
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("a", "b");
+        map.put("ww", "bee");
+        map.put("wea", "vvvb");
+        Set<String> set = map.keySet();
+//        for (int i = 0; i < set.size(); i++) {
+//            System.out.println(set.);
+//        }
+        Iterator<String> iterator = set.iterator();
+        while (iterator.hasNext()){
+            String next = iterator.next();
+            System.out.println(next);
+        }
+
+//        LinkedList<String> linkedList = new LinkedList<>();
+//        linkedList.add("222");
+//        linkedList.add("333");
+//        for (int i = 0; i < linkedList.size(); i++) {
+//
+//            String s = linkedList.get(0);
+//            System.out.println(s);
+//        }
+//        Set<Map.Entry<Integer, Integer>> entries = map.entrySet();
 
 //        Stu s = new Stu();
 //        s.setAge(1);
@@ -19,9 +44,9 @@ public class Main {
 //        map.put(s, "b");
 //        System.out.println(map.get(s));
 
-        for (int i = 1; i <= 16; i++) {
-            map.put(i, i);
-        }
+//        for (int i = 1; i <= 16; i++) {
+//            map.put(i, i);
+//        }
 
 
 //        System.out.println(map);
--- src/main/java/thread/Main.java
+++ src/main/java/thread/Main.java
@@ -1,10 +1,10 @@
 package thread;
 
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.*;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
 
 
 //        Ticket ticket = new Ticket();
@@ -16,23 +16,92 @@ public class Main {
 
         //同时启动1000个线程，去进行i++计算，看看实际结果
 
-        int num = 1000;
-        final CountDownLatch countDownLatch = new CountDownLatch(num);
+//        int num = 1000;
+//        final CountDownLatch countDownLatch = new CountDownLatch(num);
+//
+//
+//        for (int i = 0; i < num; i++) {
+//            new Thread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    Counter.inc();
+//                    countDownLatch.countDown();
+//                }
+//            }).start();
+//        }
+//
+//        countDownLatch.await();
+//        //这里每次运行的值都有可能不同,可能为1000
+//        System.out.println("运行结果:Counter.count=" + Counter.count);
 
 
-        for (int i = 0; i < num; i++) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    Counter.inc();
-                    countDownLatch.countDown();
+////        MyThread1 myThread1 = new MyThread1();
+//        Runnable runnable = new MyRunable2();
+////        Future future = new FutureTask(runnable);
+////        myThread1.start();
+////        System.out.println("other things....");
+//
+//
+//        Thread tt = new Thread(runnable);
+//        tt.start();
+////        Future<?> submit = exec.submit(runnable);
+//        exec.shutdown();
+        ExecutorService exec = Executors.newCachedThreadPool();
+//        exec.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("start");
+//                try {
+//                    Thread.sleep(5000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                System.out.println("end");
+//            }
+//        });
+
+//        Future<?> submit = exec.submit(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("start");
+//                try {
+//                    Thread.sleep(5000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                System.out.println("end");
+//            }
+//        });
+        Future<String> future = exec.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println("begin");
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            }).start();
-        }
+                System.out.println("end");
+                return "myCallable return";
+            }
+        });
+        System.out.println("other things");
+//        System.out.println(future.get()+"------------------------");
+        System.out.println("*****************");
+        exec.shutdown();
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("start");
+//                try {
+//                    Thread.sleep(5000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                System.out.println("end");
+//            }
+//        }).start();
 
-        countDownLatch.await();
-        //这里每次运行的值都有可能不同,可能为1000
-        System.out.println("运行结果:Counter.count=" + Counter.count);
-
+//        System.out.println("------");
     }
 }
