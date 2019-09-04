6a09513a9d610d8391267769102e278a43e7e6a0
fangfujing@aliyun.com <fangfujing@aliyun.com>
2019-09-04 15:30:30 +0800

    Revert "test4"
    This reverts commit 81dd322

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        final String name = "aa";
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                name.equals("");
+            }
+        }).start();
+
+        System.out.println("---");
+
+    }
+}
